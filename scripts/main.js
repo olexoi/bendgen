1711501bb41a8a1af09bebd486c118f899772fc2
olexoi <opoplavskiy64@gmail.com>
2022-06-20 17:35:38 +0300

    new version 2.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,8 @@
 $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop: true,
-        margin: 10,
-        nav: false,
+        items: 1,
+        margin:1000,
         autoplay: true,
-        autoplayTimeout: 3000,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
     })
 });
